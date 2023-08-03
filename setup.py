e059f1ddb0c173696e92b3bc143d147a3baaf743
ktroule <>
2023-08-03 15:42:42 +0000

    Add setup.py required for pip installation

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='sctools',
+    version='0.0.0',    
+    description='Single-cell utilities',
+    url='https://github.com/ktroule/sctools',
+    author='Kevin',
+    author_email='',
+    license='',
+    packages=['sctools'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
